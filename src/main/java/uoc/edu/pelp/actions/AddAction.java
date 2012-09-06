3a247d931f988600d14f83b6158cf77677c7366a
Juan Francisco Sanchez <jsanchezramos@uoc.edu>
2012-09-06 12:44:41 +0200

    Add actions pelp

--- src/main/java/uoc/edu/pelp/actions/AddAction.java
+++ src/main/java/uoc/edu/pelp/actions/AddAction.java
@@ -1,7 +1,7 @@
 /**
  * 
  */
-package uoc.edu.pelp.actions.example;
+package uoc.edu.pelp.actions;
 
 import org.apache.struts2.convention.annotation.Action;
 import org.apache.struts2.convention.annotation.Result;
